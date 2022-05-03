afb7013e7d68e1075cede2c023c929c198ed2a63
Marcellia <ankitaz.az@gmail.com>
2022-05-02 21:11:13 -0400

    Log in page created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { ChatEngine } from "react-chat-engine";
-import './App.css'
-import ChatFeed from "./components/ChatFeed";
+import { ChatEngine } from 'react-chat-engine';
 
-import React from 'react';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
 
-function App() {
-  return ( <ChatEngine
-  height = "100vh"
-  projectID="3ffc7686-34a7-4403-8e9e-a504c3f5a92f"
-  userName="azaveri"
-  userSecret="123123"
-  renderChatFeed= {(chatAppProps)=> <ChatFeed {... chatAppProps}/>}
- />
+const projectID = '3ffc7686-34a7-4403-8e9e-a504c3f5a92f';
 
-);
-  } 
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
